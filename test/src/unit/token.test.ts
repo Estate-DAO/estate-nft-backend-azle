246d9238ae38ffa940cc848d35673b26b5cd0628
Shrey ICP <shreyanshjain@estatedao.org>
2024-03-21 03:46:27 +0530

    add unit tests for token

---
+++ test/src/unit/token.test.ts
@@ -0,0 +1,114 @@
+import { generateRandomIdentity } from "@hadronous/pic";
+import { estateDaoActor, initEstateDaoNft } from "../utils/pocket-ic";
+import { OkResult } from "../utils/common";
+
+describe("Token", () => {
+  let actor: estateDaoActor;
+  const { setup, teardown } = initEstateDaoNft();
+  const alice = generateRandomIdentity();
+  const bob = generateRandomIdentity();
+  const cleo = generateRandomIdentity();
+  let mintedTokenId: bigint;
+
+  beforeAll(async () => {
+    actor = await setup();
+    actor.setIdentity(bob);
+
+    const mintRes = await actor.mint([{ subaccount: [] }]);
+    mintedTokenId = (mintRes[0][0] as OkResult).Ok;
+
+    actor.setIdentity(alice);
+  });
+
+  afterAll(teardown);
+
+  describe("icrc7_transfer", () => {
+    it("Unauthorized user fails", async () => {
+      actor.setIdentity(alice);
+      const transferRes = await actor.icrc7_transfer([
+        {
+          to: {
+            owner: cleo.getPrincipal(),
+            subaccount: [],
+          },
+          from_subaccount: [],
+          token_id: mintedTokenId,
+          memo: [],
+          created_at_time: [],
+        },
+      ]);
+  
+      expect(transferRes).toHaveLength(1);
+      expect(transferRes[0]).toHaveLength(1);
+      expect((transferRes[0][0] as any).Err.Unauthorized).toBe(null);
+    });
+
+    it("Non-existent token id fails", async () => {
+      actor.setIdentity(alice);
+      const transferRes = await actor.icrc7_transfer([
+        {
+          to: {
+            owner: cleo.getPrincipal(),
+            subaccount: [],
+          },
+          from_subaccount: [],
+          token_id: 1021n,
+          memo: [],
+          created_at_time: [],
+        },
+      ]);
+  
+      expect(transferRes).toHaveLength(1);
+      expect(transferRes[0]).toHaveLength(1);
+      expect((transferRes[0][0] as any).Err.NonExistingTokenId).toBe(null);
+    });
+
+    it("Invalid recipient fails", async () => {
+      actor.setIdentity(bob);
+      const transferRes = await actor.icrc7_transfer([
+        {
+          to: {
+            owner: bob.getPrincipal(),
+            subaccount: [],
+          },
+          from_subaccount: [],
+          token_id: mintedTokenId,
+          memo: [],
+          created_at_time: [],
+        },
+      ]);
+  
+      expect(transferRes).toHaveLength(1);
+      expect(transferRes[0]).toHaveLength(1);
+      expect((transferRes[0][0] as any).Err.InvalidRecipient).toBe(null);
+    });
+  });
+
+  describe("burn", () => {
+    it("Unauthorized user fails", async () => {
+      actor.setIdentity(alice);
+      const burnRes = await actor.burn([
+        {
+          token_id: mintedTokenId,
+        },
+      ]);
+  
+      expect(burnRes).toHaveLength(1);
+      expect(burnRes[0]).toHaveLength(1);
+      expect((burnRes[0][0] as any).Err.Unauthorized).toBe(null);
+    });
+
+    it("Non-existent token id fails", async () => {
+      actor.setIdentity(alice);
+      const burnRes = await actor.burn([
+        {
+          token_id: 1021n,
+        },
+      ]);
+  
+      expect(burnRes).toHaveLength(1);
+      expect(burnRes[0]).toHaveLength(1);
+      expect((burnRes[0][0] as any).Err.NonExistingTokenId).toBe(null);
+    });
+  });
+});
