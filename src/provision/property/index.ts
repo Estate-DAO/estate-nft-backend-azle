0447b1d393a63487629927a0e02df86897e2c64f
shrey-icp <167160449+shrey-icp@users.noreply.github.com>
2024-05-02 11:47:59 +0530

    update list_properties schema to return canister ids (#7)
    * return canister ids in list_properties
    
    * update list_properties to be a query call

--- src/provision/property/index.ts
+++ src/provision/property/index.ts
@@ -1,18 +1,14 @@
 import { Opt, Vec, nat } from "azle";
-import { PropertyStatus } from "../types";
-import { get_pending_requests } from "../request";
 import { iterableToArray } from "../../common/utils";
 import { RequestStore } from "../store";
+import { ListPropertiesResult } from "../types";
 
-export function list_properties(status: Opt<PropertyStatus>): Vec<nat> {
-  const filter_status = status.Some ?? PropertyStatus.Published;
-  
-  if ( 'Draft' in filter_status ) {
-    return get_pending_requests();
-  } else {
-    // Published
-    return iterableToArray(RequestStore.config.entries())
-      .filter(([id, config]) => !!config.token_canister.Some)
-      .map(([id, config]) => id)
-  }
+export function list_properties(): Vec<ListPropertiesResult> {
+  return iterableToArray(RequestStore.config.entries())
+    .filter(([id, config]) => !!config.token_canister.Some)
+    .map(([id, config]) => ({
+      id,
+      token_canister: config.token_canister.Some!,
+      asset_canister: config.asset_canister.Some!,
+    }));
 }
