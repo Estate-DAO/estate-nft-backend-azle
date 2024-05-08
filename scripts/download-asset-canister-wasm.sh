d65589ce2fe22be809ee2e647e86dfa47af1f210
shrey-icp <shreyanshjain@estatedao.org>
2024-05-09 02:03:43 +0530

    add asset proxy file upload script

--- scripts/download-asset-canister-wasm.sh
+++ scripts/download-asset-canister-wasm.sh
@@ -4,7 +4,7 @@ cd asset-canister
 
 WASM_NAME=assetstorage.wasm.gz
 if [ ! -f $WASM_NAME ]; then
-  wget https://github.com/dfinity/sdk/raw/0.16.0/src/distributed/assetstorage.wasm.gz -O assetstorage.wasm.gz
+  wget https://github.com/dfinity/sdk/raw/0.19.0/src/distributed/assetstorage.wasm.gz -O assetstorage.wasm.gz
 else
   echo "Asset Canister Wasm already exists"
 fi
---
+++ scripts/file-upload-asset-proxy.ts
@@ -0,0 +1,23 @@
+import { readFile } from "fs/promises";
+import { AssetManager } from "@dfinity/assets";
+import { getAgent, getCanisterId } from "./common";
+
+async function main() {
+  const assetProxyManager = new AssetManager({
+    canisterId: await getCanisterId('asset_proxy'),
+    agent: await getAgent(),
+  });
+ 
+  const assetManager = new AssetManager({
+    canisterId: await getCanisterId('asset'),
+    agent: await getAgent(),
+  });
+ 
+  const file = await readFile('./test.jpg');
+  console.log(await assetProxyManager.store('./test.jpg', {
+    contentEncoding: 'identity'
+  }));
+  console.log(await assetManager.get('/test.jpg', ['identity']))
+}
+
+main();
