ea0e4ad99ef9f9cc76cfb905b3f8ecc67bc422c4
shrey-icp <shreyanshjain@estatedao.org>
2024-07-07 19:05:20 +0530

    add index canister

--- src/common/ledger.ts
+++ src/common/ledger.ts
@@ -1,8 +1,8 @@
 import { Principal } from "azle";
-import { Ledger } from "azle/canisters/ledger";
+import LedgerIndex from "../icp_ledger_index/index";
 
 export const TRANSFER_FEE = 10_000n;
 
 export function getTokenLedger(principal: Principal) {
-  return Ledger(principal);
+  return LedgerIndex(principal);
 }
